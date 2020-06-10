c8740e99ebb50ee02db4439c4d34b4eb4728fc1a
kitmikai <matara.timothy@gmail.com>
2020-06-11 01:44:55 +0300

    Complete script to write to HTML

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ var concepts = ["JavaScript", "Operators", "Variables", "Variable naming convent
 
 var definitions = ["A web scripting language", "mathematical operators, logical operators", "A temporary store for data in computer memory", "Begin with lower case only, camelCase", "Break tasks into small pieces", "Anything in quotes, string methods","Operations that do not return anything", "What the function works with", "What you pass to a function","A keyword","object.method().anothermethod()", "True or False", "Undefined","A datatype returned when result is not a number", "escape characters for newline, tab", "A pop up window", "Ignored by the JavaScript Engine", "A JavaScript library", "Features within HTML tags"];
 
+// Create card object
 function Card(question, answer){
 	this.question = question;
   this.answer = answer;
@@ -18,25 +19,18 @@ Card.prototype.getAnswer = function(){
 }
 
 // loop through the arrays
-for(let counter=0; counter <= concepts.length; counter++){
+
+for(let counter=0; counter < concepts.length; counter++){
 	var cardQuestion = concepts[counter];
     var cardAnswer = definitions[counter];
-  
-  //console.log(cardQuestion,cardAnswer);
-  
+
   // Create card name
     var cardName = "card" + counter;
-  
-  //console.log(cardname);
   
   // Instantiate object
     cardName = new Card(cardQuestion, cardAnswer);
 
-    cardObject = document.querySelector(".card-groups").innerHTML = "<div class=\"card\">" + cardName.getQuestion + "</div>" ;
-    //cardObject.innerHTML
-  
-	//console.log(cardName.getQuestion(), cardName.getAnswer());
-  //document.write = "<div class=\"card\">" + cardName.getQuestion() + "</div>";
-	//console.log(card1.getAnswer());
+    // Create flashcard in the DIV
+    document.querySelector(".card-groups").innerHTML += "<div class=\"card\"> <div class=\"front\"> "  + cardName.getQuestion() + "</div> <div class=\"back\">" + cardName.getAnswer() + "</div></div>" ;
   
 }
