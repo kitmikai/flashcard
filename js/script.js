5b17fa3a3b0c22f29406d8c9ce17c1c849aa780d
kitmikai <matara.timothy@gmail.com>
2020-06-11 10:08:58 +0300

    Indenting code and fixing typos

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var concepts = ["JavaScript", "Operators", "Variables", "Variable naming conventions", "Functions", "Strings", "Methods", "Arguments", "Parameters", "Return", "Chaining methods", "Booleans", "Undefined", "NaN", "Escape", "Alert", "Comments", "jQuery", "Attributes"];
 
-var definitions = ["A web scripting language", "mathematical operators, logical operators", "A temporary store for data in computer memory", "Begin with lower case only, camelCase", "Break tasks into small pieces", "Anything in quotes, string methods","Operations that do not return anything", "What the function works with", "What you pass to a function","A keyword","object.method().anothermethod()", "True or False", "Undefined","A datatype returned when result is not a number", "escape characters for newline, tab", "A pop up window", "Ignored by the JavaScript Engine", "A JavaScript library", "Features within HTML tags"];
+var definitions = ["A web scripting language known as Ecma Script used to make responsive web pages", "Math operators: + - % / *,<br> Logical operators: &&, ||, >=, <=", "A temporary store for data in computer memory", "Must begin with lower case characters, <br> camelCase<br> cannot be a keyword", " A JavaScript procedure—a set of statements that performs a task or calculates a value", "A series of characters enclosed in quotes","Operations that do not return anything", "What the function works with", "What you pass to a function"," Stops the execution of a function. If specified, a given value is returned to the function caller","object.method().anothermethod()", "True or False", "Undefined","A datatype returned when result is not a number", "escape characters for newline, tab", "A pop up window", "Line of code that is ignored by using // at the start of a statement", "A JavaScript library", "Additional characteristics or properties of the element such as width and height of an image"];
 
 // Create card object
 function Card(question, answer){
@@ -8,12 +8,12 @@ function Card(question, answer){
   this.answer = answer;
 }
 
-// getQuestion
+// getQuestion prototype
 Card.prototype.getQuestion = function(){
 	return `${this.question}`;
 }
 
-// getAnswer
+// getAnswer prototype
 Card.prototype.getAnswer = function(){
 	return `${this.answer}`;
 }
@@ -21,16 +21,17 @@ Card.prototype.getAnswer = function(){
 // loop through the arrays
 
 for(let counter=0; counter < concepts.length; counter++){
-	var cardQuestion = concepts[counter];
-    var cardAnswer = definitions[counter];
+  var cardQuestion = concepts[counter];
+  var cardAnswer = definitions[counter];
 
-  // Create card name
-    var cardName = "card" + counter;
+  // Create unique card name
+  var cardName = "card" + counter;
   
-  // Instantiate object
-    cardName = new Card(cardQuestion, cardAnswer);
+  // Instantiate card object with question and answer
+  cardName = new Card(cardQuestion, cardAnswer);
 
-    // Create flashcard in the DIV
-    document.querySelector(".card-groups").innerHTML += "<div class=\"card\"> <div class=\"front\"> "  + cardName.getQuestion() + "</div> <div class=\"back\">" + cardName.getAnswer() + "</div></div>" ;
+  // Create flashcard in the DIV
+  // Modify to implement JQuery
+  document.querySelector(".card-groups").innerHTML += "<div class=\"card\"> <div class=\"front\"> "  + cardName.getQuestion() + "</div> <div class=\"back\">" + cardName.getAnswer() + "</div></div>" ;
   
 }
